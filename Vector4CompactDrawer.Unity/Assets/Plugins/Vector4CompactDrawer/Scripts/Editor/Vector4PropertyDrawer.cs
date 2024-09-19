5a80db70352af4df8d51e03284b9edd03b451e56
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2024-09-19 15:23:22 +0300

    Create vector4 property drawer

---
+++ Vector4CompactDrawer.Unity/Assets/Plugins/Vector4CompactDrawer/Scripts/Editor/Vector4PropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Vector4CompactDrawer.Editor
+{
+    [CustomPropertyDrawer(typeof(Vector4))]
+    public class Vector4PropertyDrawer : PropertyDrawer
+    {
+        private static readonly string[] _fieldNames =
+            { nameof(Vector4.x), nameof(Vector4.y), nameof(Vector4.z), nameof(Vector4.w) };
+
+        private static readonly GUIContent[] _fieldLabels =
+            _fieldNames.Select(fieldName => new GUIContent(fieldName.ToUpper())).ToArray();
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.Next(true);
+            EditorGUI.MultiPropertyField(position, _fieldLabels, property, label);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) =>
+            EditorGUIUtility.singleLineHeight +
+            (EditorGUIUtility.wideMode ? default : EditorGUIUtility.singleLineHeight);
+    }
+}
