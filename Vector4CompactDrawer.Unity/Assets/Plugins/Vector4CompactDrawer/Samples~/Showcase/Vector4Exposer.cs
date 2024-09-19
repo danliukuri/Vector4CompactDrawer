eb80cc75d51cfea4c0c7a3c697e8cc5d6cbecca1
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2024-09-19 18:24:31 +0300

    Create Vector4 property drawer (#2)
    * Create plugin showcase sample
    
    * Create Vector4 property drawer
    
    * Add default properties right click menu

---
+++ Vector4CompactDrawer.Unity/Assets/Plugins/Vector4CompactDrawer/Samples~/Showcase/Vector4Exposer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Vector4CompactDrawer.Samples.Showcase
+{
+    public class Vector4Exposer : MonoBehaviour
+    {
+        [SerializeField] private Vector4 serializedVector;
+    }
+}
